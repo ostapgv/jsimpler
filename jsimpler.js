94b1311575de1ff96abaaf0545bd11ee7d81016d
Grigory Ostapenko <gmostapgv@gmail.com>
2015-11-25 18:04:40 +0100

    added tests and functionality for the class and dom manipulations

--- jsimpler.js
+++ jsimpler.js
@@ -15,10 +15,36 @@ var JSimpler = (function() {
     return new JSimpler.fn.init(selector);
   }
 
-  // Common abstract methods starts with "_". They dont use main object
+  // ### Common abstract methods starts with "_" ###
+
+  // Check if the HTML element name is valid
   var _isValidElementName = function(elementName) {
     return document.createElement(elementName).toString() != "[object HTMLUnknownElement]";
   }
+
+  // Add content into each mached elements, using insertion rules (insertBefore/appendChild)
+  var _addContent = function(self, content, action) {
+    return self.each(function(that, e, i) {
+      // 1 - ELEMENT_NODE, 9 - DOCUMENT_NODE, 11 - DOCUMENT_FRAGMENT_NODE
+      if (e.nodeType === 1 || e.nodeType === 9 || e.nodeType === 11) {
+        // The second parameter is only for the insertBefore(). It doesn't affect to the appendChild().
+        e[action](content, e.firstElementChild);
+      }
+    });
+  }
+
+  // Get next/prev elements
+  var _getElement = function(self, action) {
+    var match = [];
+    self.each(function(that, e, i) {
+      if(e[action] != null) {
+        match = [e[action]];
+        return;
+      }
+    });
+    return match;
+  }
+
 
   JSimpler.fn = JSimpler.prototype = {
 
@@ -92,7 +118,7 @@ var JSimpler = (function() {
     // Executes callback for each selector
     each: function(callback) {
       for(var i=0, l=this.selection.length; i<l; i++) {
-        callback(i, this.selection[i]);
+        callback(this, this.selection[i], i);
       };
       return this;
     },
@@ -109,37 +135,31 @@ var JSimpler = (function() {
       }
     },
 
-    // Appending elements to the current object
-    // TODO This funktion doesn't work correctly now. Fix it.
-    append: function(selector) {
-      var curSelection;
-      if(selector instanceof JSimpler){
-        curSelection = selector;
-      } else {
-        curSelection = new JSimpler.fn.init(selector);
-      }
-      this.each(function(i, e) {
-        e.get().appendChild(curSelection.get())
-      });
-      return this;
+    // Get the immediately previous sibling
+    prev: function() {
+      return _getElement(this, "previousElementSibling");
+    },
+
+    // Get the immediately following sibling
+    next: function() {
+      return _getElement(this, "nextElementSibling");
     },
 
     // Set property to the element
     prop: function(name, value) {
-      this.each(function(i, e) {
+      return this.each(function(self, e, i) {
         e.setAttribute(name, value);
       });
-      return this;
     },
 
     css: function(obj, value) {
       if(typeof obj === "string" && typeof value === "string") {
-        this.each(function(i, e) {
+        return this.each(function(self, e, i) {
           e.style[obj] = value;
         });
       // If only the first parameter passed it should be an Object
       } else if (typeof obj === "object" && typeof value === "undefined"){
-        this.each(function(i, e) {
+        return this.each(function(self, e, i) {
           for(var key in obj) {
             e.style[key] = obj[key];
           }
@@ -150,7 +170,96 @@ var JSimpler = (function() {
         }
       }
       return this;
+    },
+
+    // Add class to selected elements
+    addClass: function(className) {
+      return this.each(function(self, e, i) {
+        e.classList.add(className);
+      });
+    },
+
+    // Removes the specified class by the className
+    removeClass: function(className) {
+      return this.each(function(self, e, i) {
+        e.classList.remove(className);
+      });
+    },
+
+    // Toggle the specified class by the className
+    toggleClass: function(className) {
+      return this.each(function(self, e, i) {
+        e.classList.toggle(className);
+      });
+    },
+
+    // Check if the className is presented in selected elements
+    hasClass: function(className) {
+      className = className.trim();
+      var classArr;
+      var match = false;
+      this.each(function(self, e, i) {
+        classArr = e.className.split(" ");
+        if(classArr.indexOf(className) >= 0) {
+          match = true;
+          return;
+        }
+      });
+      return match;
+    },
+
+    // Removes set of elements
+    remove: function() {
+      return this.each(function(self, e, i) {
+        // If element has already been deleted
+        if(e) {
+          e.remove();
+          while (self.length--) {
+            delete self[self.length];
+          }
+          self.length = 0;
+          self.selection = [];
+        }
+      });
+    },
+
+    // Add the content to the BEGINING of each element in the set of matched elements
+    prepend: function(content) {
+      return _addContent(this, content, "insertBefore");
+    },
+
+    // Add the content to the END of each element in the set of matched elements
+    append: function(content) {
+      return _addContent(this, content, "appendChild");
+      //var curSelection;
+
+      /*
+      if(selector instanceof JSimpler){
+        curSelection = selector;
+      } else {
+        curSelection = new JSimpler.fn.init(selector);
+      }
+      this.each(function(self, e, i) {
+        e.appendChild(curSelection.get())
+      });
+      return this;
+      */
+    },
+
+    // Insert content, specified by the parameter, before each element in the set of matched elements.
+    before: function(content) {
+      return this.each(function(that, e, i) {
+        e.parentNode.insertBefore(content, e.previousSibling);
+      });
+    },
+
+    // Insert content, specified by the parameter, after each element in the set of matched elements.
+    after: function(content) {
+      return this.each(function(that, e, i) {
+        e.parentNode.insertBefore(content, e.nextSibling);
+      });
     }
+
 
   };
   // Setting up newly created object prototype to the wrapper's protopype
@@ -161,7 +270,7 @@ var JSimpler = (function() {
 })();
 
 // Example of extending usage
-JSimpler.prototype.extend({
+JSimpler.fn.extend({
   newMethod: function() {
     // do something
   }
--- spec/JSimplerSpec.js
+++ spec/JSimplerSpec.js
@@ -1,18 +1,23 @@
 describe('JSimpler', function() {
+  'use strict';
+
+  var J = JSimpler;
+  var $selectedElement;
+  var selectedElement;
 
   it('should be defined', function() {
-    expect(JSimpler).toBeDefined();
+    expect(J).toBeDefined();
   });
 
+
   describe('Selector', function() {
-    'use strict';
 
     it('should select an empty array if the element does not exist in DOM', function() {
       var selector = '.some-element-not-in-the-dom';
 
       // We're calling $.makeArray because we need to transform jQuery result set into real array object
       var expectedSelectedElement = $.makeArray($(selector));
-      var selectedElements = $.makeArray(JSimpler(selector));
+      var selectedElements = $.makeArray(J(selector));
 
       expect(selectedElements).toEqual(expectedSelectedElement);
       expect(selectedElements.length).toBe(0);
@@ -21,7 +26,7 @@ describe('JSimpler', function() {
     it('should select a DOM element with given ID', function() {
       var id = 'divId';
       var expectedSelectedElement = $.makeArray($('#' + id));
-      var selectedElement = $.makeArray(JSimpler('#' + id));
+      var selectedElement = $.makeArray(J('#' + id));
 
       expect(selectedElement).toEqual(expectedSelectedElement);
       expect(selectedElement.length).toBe(1);
@@ -33,7 +38,7 @@ describe('JSimpler', function() {
     it('should select DOM elements with a given class name', function() {
       var className = '.infinum';
       var expectedHTMLElementsArray = $.makeArray($(className));
-      var selectedElementsArray = $.makeArray(JSimpler(className));
+      var selectedElementsArray = $.makeArray(J(className));
 
       expect(selectedElementsArray.length).toBe(expectedHTMLElementsArray.length);
 
@@ -46,7 +51,7 @@ describe('JSimpler', function() {
     it('should select DOM elements with a given tag name', function() {
       var tagName = 'p';
       var expectedHTMLElementsArray = $.makeArray($(tagName));
-      var selectedElementsArray = $.makeArray(JSimpler(tagName));
+      var selectedElementsArray = $.makeArray(J(tagName));
 
       expect(selectedElementsArray.length).toBe(expectedHTMLElementsArray.length);
 
@@ -57,7 +62,7 @@ describe('JSimpler', function() {
 
     it('should throw an expection for invalid selector', function() {
       expect(function() {
-        JSimpler(')(?/');
+        J(')(?/');
       }).toThrow();
     });
 
@@ -65,13 +70,9 @@ describe('JSimpler', function() {
 
 
   describe('.css()', function() {
-    var J = JSimpler;
-    var $selectedElement;
-    var selectedElement;
 
     beforeEach(function() {
-      J = JSimpler;
-      $selectedElement = J('#main-part');
+      $selectedElement = J('#mainPart');
       selectedElement = $selectedElement[0];
     });
 
@@ -86,8 +87,8 @@ describe('JSimpler', function() {
     });
 
     it('should return an existing CSS property value of an HTML element', function() {
-      $selectedElement.css('display', 'none');
-      expect($selectedElement.css('display')).toBe('none');
+      $selectedElement.css('display', 'block');
+      expect($selectedElement.css('display')).toBe('block');
     });
 
     it('should set multiple CSS properties of an HTML element', function() {
@@ -120,6 +121,133 @@ describe('JSimpler', function() {
 
       expect($firstLi.css('color')).toBe('black');
       expect($li.css('color')).toBe('black');
+    });
+
+  });
+
+
+  describe('class manipulations (addClass, removeClass, toggleClass, hasClass)', function() {
+
+    beforeEach(function() {
+      $selectedElement = J('#mainPart');
+      selectedElement = $selectedElement[0];
+    });
+
+    it('can add a class to the element', function() {
+      $selectedElement.addClass('building');
+      expect($selectedElement.hasClass('building')).toBe(true);
+    });
+
+    it('should not overwrite existing css classes', function() {
+      $selectedElement.addClass('spooky');
+      $selectedElement.addClass('building');
+
+      expect($selectedElement.hasClass('building')).toBe(true);
+      expect($selectedElement.hasClass('spooky')).toBe(true);
+      expect($selectedElement.hasClass('no-such-class-here')).toBe(false);
+    });
+
+    it('should remove a specific css class of the element', function() {
+      $selectedElement.addClass('very-important-class');
+      $selectedElement.addClass('super-cool-class');
+
+      $selectedElement.removeClass('super-cool-class');
+
+      expect($selectedElement.hasClass('super-cool-class')).toBe(false);
+      expect($selectedElement.hasClass('very-important-class')).toBe(true);
+    });
+
+    it('should toggle a css class of the element', function() {
+      $selectedElement.addClass('hidden-tower');
+
+      $selectedElement.toggleClass('hidden-tower');
+      expect($selectedElement.hasClass('hidden-tower')).toBe(false);
+
+      $selectedElement.toggleClass('hidden-tower');
+      expect($selectedElement.hasClass('hidden-tower')).toBe(true);
+    });
+
+    it('should return true if a HTML element has a given css class', function() {
+      $selectedElement.addClass('hidden-tower');
+      expect($selectedElement.hasClass('hidden-tower')).toBe(true);
+    });
+
+    it('should return false if a HTML element doesn\'t have a given css class', function() {
+      $selectedElement.removeClass('hidden-tower');
+      expect($selectedElement.hasClass('hidden-tower')).toBe(false);
+    });
+
+  });
+
+
+  describe('dom manipulation', function() {
+    var $selectedElement, selectedElement;
+
+    beforeEach(function() {
+      $selectedElement = J('#domManipulations');
+      selectedElement = $selectedElement[0];
+    });
+
+    it('should be able to remove a HTML element', function() {
+      var $toRemoveElement = J('#toRemove');
+      var toRemoveElement = $toRemoveElement[0];
+
+      expect(document.contains(toRemoveElement)).toBe(true);
+      $toRemoveElement.remove();
+      expect(document.contains(toRemoveElement)).toBe(false);
+    });
+
+    it('should correctly remove nested elements', function() {
+      var parentElement = document.createElement('div');
+      var childElement = document.createElement('div');
+
+      parentElement.appendChild(childElement);
+      selectedElement.appendChild(parentElement);
+
+      var bothElements = J("#domManipulations div");
+      bothElements.remove();
+
+      expect(bothElements.length).toBe(0);
+    });
+
+    it('should append a HTML element to the given element', function() {
+      var newElement = document.createElement('h4');
+      var initialChildrenCount = selectedElement.children.length;
+
+      expect(initialChildrenCount).toBeGreaterThan(0);
+
+      $selectedElement.append(newElement);
+
+      expect(selectedElement.children.length).toBe(initialChildrenCount + 1);
+      expect(selectedElement.children[initialChildrenCount]).toBe(newElement);
+    });
+
+    it('should prepend a HTML element to the given element', function() {
+      var newElement = document.createElement('h4');
+      var initialChildrenCount = selectedElement.children.length;
+
+      expect(initialChildrenCount).toBeGreaterThan(0);
+
+      $selectedElement.prepend(newElement);
+
+      expect(selectedElement.children.length).toBe(initialChildrenCount + 1);
+      expect(selectedElement.children[0]).toBe(newElement);
+    });
+
+    it('should be able to add a new HTML element after a given HTML element', function() {
+      var newElement = document.createElement('div');
+
+      expect($selectedElement.next()[0]).not.toBe(newElement);
+      $selectedElement.after(newElement);
+      expect($selectedElement.next()[0]).toBe(newElement);
+    });
+
+    it('should be able to add a new HTML element before a given HTML element', function() {
+      var newElement = document.createElement('p');
+
+      expect($selectedElement.prev()[0]).not.toBe(newElement);
+      $selectedElement.before(newElement);
+      expect($selectedElement.prev()[0]).toBe(newElement);
     });
 
   });
